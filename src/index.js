f59a52b61faa6209197644aa2d2cf60e1996c671
Samkevich <D:\The Bat\E-mail>
2017-09-26 16:00:54 +0300

    all done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	let i = 0;
+	let resultArray = [];
+	let arrayBrackets = str.slice('');
+	
+	while (i < arrayBrackets.length) {
+		for (let j = 0; j < bracketsConfig.length; j++) {
+			if ((resultArray[resultArray.length - 1] === bracketsConfig[j][0]) 
+				&& (arrayBrackets[i] === bracketsConfig[j][1])) {
+				resultArray.pop();
+			} else if (arrayBrackets[i] === bracketsConfig[j][0]) {
+				resultArray.push(arrayBrackets[i]);
+			}
+		}
+		i++;
+	}
+
+	if (resultArray.length === 0) {
+		return true;
+    } else {
+    	return false;
+    }
 }
